92818c0277ca53c3dc233f60b60027fce5f660a5
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-04-04 20:28:12 +0200

    removed mobile button for now

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -104,3 +104,18 @@ export default function Navbar() {
     </motion.div>
   );
 }
+
+// function MobileMenuButton() {
+//   const [isOpen, setIsOpen] = useState(false)
+
+//   return (
+//     <button className="text-gray-500 w-10 h-10 relative focus:outline-none bg-white" onClick={() => set}>
+//                 <span className="sr-only">Open main menu</span>
+//                 <div className="block w-5 absolute left-1/2 top-1/2   transform  -translate-x-1/2 -translate-y-1/2">
+//                     <span aria-hidden="true" className="block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out" :class="{'rotate-45': open,' -translate-y-1.5': !open }"></span>
+//                     <span aria-hidden="true" className="block absolute  h-0.5 w-5 bg-current   transform transition duration-500 ease-in-out" :class="{'opacity-0': open } "></span>
+//                     <span aria-hidden="true" className="block absolute  h-0.5 w-5 bg-current transform  transition duration-500 ease-in-out" :class="{'-rotate-45': open, ' translate-y-1.5': !open}"></span>
+//                 </div>
+//             </button>
+//   )
+// }
