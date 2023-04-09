4cdb1802e826fb22c8739991d0e481415f9d2ab1
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-04-09 22:46:55 +0200

    added radiogroup

--- src/pages/product/[title_id].tsx
+++ src/pages/product/[title_id].tsx
@@ -5,6 +5,8 @@ import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 import { NextPage, type GetStaticProps } from "next";
 import { prisma } from "~/server/db";
 import { log } from "console";
+import { RadioGroup } from "@headlessui/react";
+import { useState } from "react";
 
 export const getStaticPaths = async () => {
   const products = await prisma.product.findMany({
@@ -27,7 +29,6 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = generateSSGHelper();
-
   const title_id = context.params?.title_id;
 
   if (typeof title_id !== "string") {
@@ -45,6 +46,8 @@ export const getStaticProps: GetStaticProps = async (context) => {
 };
 
 const ProductPage: NextPage<{ title_id: string }> = ({ title_id }) => {
+  const [color, setColor] = useState("red");
+
   const { data: product } = api.product.getProductByTitle.useQuery({
     title_id,
   });
@@ -71,10 +74,47 @@ const ProductPage: NextPage<{ title_id: string }> = ({ title_id }) => {
               />
             </div>
             <div className="my-auto flex w-full flex-col gap-6 md:w-2/5 md:pl-6">
-              <h2 className="pb-4 text-3xl  font-medium md:text-4xl ">
+              <h2 className=" text-3xl  font-medium md:text-4xl ">
                 {product.title}
               </h2>
-              <p className=" font-light text-gray-300">{product.description}</p>
+
+              <div>
+                {/* {product.colors.map((color) => (
+                    <div
+                      key={color}
+                      className="h-8 w-8 rounded-full"
+                      style={{ backgroundColor: color }}
+                    ></div>
+                  ))} */}
+                <Radio label="color" selected={color} setSelected={setColor}>
+                  <>
+                    {product.colors.map((color) => (
+                      <RadioGroup.Option
+                        key={color}
+                        value={color}
+                        className={({ active, checked }) =>
+                          `
+                  ${
+                    checked
+                      ? " ring-2 ring-bunker-300 ring-offset-2 ring-offset-bunker-950"
+                      : ""
+                  }
+                    relative flex cursor-pointer rounded-full shadow-md   focus:outline-none`
+                        }
+                      >
+                        {({ active, checked }) => (
+                          <>
+                            <div
+                              className="h-8 w-8 rounded-full"
+                              style={{ backgroundColor: color }}
+                            ></div>
+                          </>
+                        )}
+                      </RadioGroup.Option>
+                    ))}
+                  </>
+                </Radio>
+              </div>
 
               <Divider />
               <div className="flex justify-between ">
@@ -107,3 +147,71 @@ const ProductPage: NextPage<{ title_id: string }> = ({ title_id }) => {
 };
 
 export default ProductPage;
+
+function Radio({
+  label,
+  children,
+  selected,
+  setSelected,
+}: {
+  label: string;
+  children: React.ReactNode;
+  selected: string;
+  setSelected: (value: string) => void;
+}) {
+  return (
+    <div className="w-full ">
+      <div className="mx-auto w-full max-w-md">
+        <RadioGroup value={selected} onChange={setSelected}>
+          <RadioGroup.Label className="">{label}</RadioGroup.Label>
+          <div className="flex gap-3 pt-3">{children}</div>
+        </RadioGroup>
+      </div>
+    </div>
+  );
+}
+
+function CheckIcon(props: React.ComponentProps<"svg">) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" {...props}>
+      <circle cx={12} cy={12} r={12} fill="#fff" opacity="0.2" />
+      <path
+        d="M7 13l3 3 7-7"
+        stroke="#fff"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+// {plans.map((plan) => (
+//   <RadioGroup.Option
+//     key={plan.name}
+//     value={plan}
+//     className={({ active, checked }) =>
+//       `${
+//         active
+//           ? "ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300"
+//           : ""
+//       }
+//       ${checked ? "ring-2 ring-bunker-800" : ""}
+//         relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none`
+//     }
+//   >
+//     {({ active, checked }) => (
+//       <>
+//         <div className="flex  items-center justify-between">
+//           <div className="flex items-center">
+//             <div className="text-sm">
+//               <RadioGroup.Label as="p" className={`font-medium  `}>
+//                 {plan.name}
+//               </RadioGroup.Label>
+//             </div>
+//           </div>
+//         </div>
+//       </>
+//     )}
+//   </RadioGroup.Option>
+// ))}
