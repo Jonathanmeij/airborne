bd068a79fe11ca4943a3df18757a4ace87e1f3ab
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-04-09 16:35:04 +0200

    ssg

---
+++ src/server/helpers/ssgHelper.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import { prisma } from "~/server/db";
+import { appRouter } from "~/server/api/root";
+import superjson from "superjson";
+import { createInnerTRPCContext } from "~/server/api/trpc";
+
+export const generateSSGHelper = () =>
+  createProxySSGHelpers({
+    router: appRouter,
+    ctx: createInnerTRPCContext({ session: null }),
+    transformer: superjson, // optional - adds superjson serialization
+  });
