6d7bf0e590b25fdf0dc4dab101550c6a214ab5ec
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-04-10 16:44:07 +0200

    added shoping cart icon to nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -49,7 +49,19 @@ export default function Navbar() {
             <Link href="/">AIRBORNE</Link>
           </h1>
 
-          <div className="md:hidden">
+          <div className="flex items-center gap-3 md:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="white"
+              width="26"
+              height="26"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="white"
+                d="M4.558 7l4.701-4.702c.199-.198.46-.298.721-.298.613 0 1.02.505 1.02 1.029 0 .25-.092.504-.299.711l-3.26 3.26h-2.883zm12.001 0h2.883l-4.701-4.702c-.199-.198-.46-.298-.721-.298-.613 0-1.02.505-1.02 1.029 0 .25.092.504.299.711l3.26 3.26zm-16.559 2v2h.643c.534 0 1.021.304 1.256.784l4.101 10.216h12l4.102-10.214c.233-.481.722-.786 1.256-.786h.642v-2h-24z"
+              />
+            </svg>
             <MobileMenu />
           </div>
 
