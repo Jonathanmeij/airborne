68d608b3b5b02b2891af4b26dd185b06b3eaae31
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-04-11 21:13:57 +0200

    fixed svg

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -16,8 +16,6 @@ export default function CartMenu() {
     }, 0);
   }
 
-  console.log(cart);
-
   return (
     <div className="">
       <Popover className="h-full">
@@ -154,10 +152,10 @@ function CartItem(CartItem: CartItem) {
             € {CartItem.Product.price * CartItem.quantity}
           </span>
           <svg
-            clip-rule="evenodd"
-            fill-rule="evenodd"
-            stroke-linejoin="round"
-            stroke-miterlimit="2"
+            clipRule="evenodd"
+            fillRule="evenodd"
+            strokeLinejoin="round"
+            strokeMiterlimit="2"
             height="24"
             width="24"
             fill="#405661"
@@ -168,7 +166,7 @@ function CartItem(CartItem: CartItem) {
           >
             <path
               d="m20.015 6.506h-16v14.423c0 .591.448 1.071 1 1.071h14c.552 0 1-.48 1-1.071 0-3.905 0-14.423 0-14.423zm-5.75 2.494c.414 0 .75.336.75.75v8.5c0 .414-.336.75-.75.75s-.75-.336-.75-.75v-8.5c0-.414.336-.75.75-.75zm-4.5 0c.414 0 .75.336.75.75v8.5c0 .414-.336.75-.75.75s-.75-.336-.75-.75v-8.5c0-.414.336-.75.75-.75zm-.75-5v-1c0-.535.474-1 1-1h4c.526 0 1 .465 1 1v1h5.254c.412 0 .746.335.746.747s-.334.747-.746.747h-16.507c-.413 0-.747-.335-.747-.747s.334-.747.747-.747zm4.5 0v-.5h-3v.5z"
-              fill-rule="nonzero"
+              fillRule="nonzero"
             />
           </svg>
         </div>
