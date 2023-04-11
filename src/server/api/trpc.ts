396af7f695eec4336a1755cbdb3e6efd19b0eb7f
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-04-11 21:14:27 +0200

    added clerk to server side

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -15,31 +15,11 @@
  * These allow you to access things when processing a request, like the database, the session, etc.
  */
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
-import { type Session } from "next-auth";
 
 import { getServerAuthSession } from "~/server/auth";
 import { prisma } from "~/server/db";
 
-type CreateContextOptions = {
-  session: Session | null;
-};
-
-/**
- * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
- * it from here.
- *
- * Examples of things you may need it for:
- * - testing, so we don't have to mock Next.js' req/res
- * - tRPC's `createSSGHelpers`, where we don't have req/res
- *
- * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
- */
-export const createInnerTRPCContext = (opts: CreateContextOptions) => {
-  return {
-    session: opts.session,
-    prisma,
-  };
-};
+type CreateContextOptions = Record<string, never>;
 
 /**
  * This is the actual context you will use in your router. It will be used to process every request
@@ -47,15 +27,17 @@ export const createInnerTRPCContext = (opts: CreateContextOptions) => {
  *
  * @see https://trpc.io/docs/context
  */
-export const createTRPCContext = async (opts: CreateNextContextOptions) => {
-  const { req, res } = opts;
+export const createTRPCContext = (opts: CreateNextContextOptions) => {
+  //request from the client
+  const { req } = opts;
+  const sesh = getAuth(req);
 
-  // Get the session from the server using the getServerSession wrapper function
-  const session = await getServerAuthSession({ req, res });
+  const userId = sesh.userId;
 
-  return createInnerTRPCContext({
-    session,
-  });
+  return {
+    prisma,
+    userId,
+  };
 };
 
 /**
@@ -68,6 +50,7 @@ export const createTRPCContext = async (opts: CreateNextContextOptions) => {
 import { initTRPC, TRPCError } from "@trpc/server";
 import superjson from "superjson";
 import { ZodError } from "zod";
+import { getAuth } from "@clerk/nextjs/server";
 
 const t = initTRPC.context<typeof createTRPCContext>().create({
   transformer: superjson,
@@ -108,13 +91,16 @@ export const publicProcedure = t.procedure;
 
 /** Reusable middleware that enforces users are logged in before running the procedure. */
 const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
-  if (!ctx.session || !ctx.session.user) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
+  if (!ctx.userId) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You must be logged in to do that.",
+    });
   }
+
   return next({
     ctx: {
-      // infers the `session` as non-nullable
-      session: { ...ctx.session, user: ctx.session.user },
+      userId: ctx.userId,
     },
   });
 });
--- src/server/helpers/ssgHelper.ts
+++ src/server/helpers/ssgHelper.ts
@@ -3,11 +3,10 @@ import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import { prisma } from "~/server/db";
 import { appRouter } from "~/server/api/root";
 import superjson from "superjson";
-import { createInnerTRPCContext } from "~/server/api/trpc";
 
 export const generateSSGHelper = () =>
   createProxySSGHelpers({
     router: appRouter,
-    ctx: createInnerTRPCContext({ session: null }),
+    ctx: { prisma, userId: null }, //add userID
     transformer: superjson, // optional - adds superjson serialization
   });
