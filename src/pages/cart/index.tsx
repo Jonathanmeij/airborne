80fdde5e2551c687b1ee009216c30f83516ad913
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-04-18 16:27:36 +0200

    added checkmark

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -500,7 +500,19 @@ function Step({ step, currentStep }: { step: number; currentStep: number }) {
       variants={stepVariants}
       animate={status}
     >
-      {step}
+      {status === "completed" ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="15"
+          height="15"
+          viewBox="0 0 24 24"
+          fill="white"
+        >
+          <path d="M9 22l-10-10.598 2.798-2.859 7.149 7.473 13.144-14.016 2.909 2.806z" />
+        </svg>
+      ) : (
+        step
+      )}
     </motion.div>
   );
 }
