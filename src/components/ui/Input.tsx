2449ed0e9cda93fc88b983c8f2d08e60eb5cf8f0
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-04-22 11:39:45 +0200

    added listbox

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { useController } from "react-hook-form";
 
@@ -101,79 +101,103 @@ export function TextArea({
 }
 
 interface ListBoxProps {
-  selected: number;
-  setSelected: (selected: number) => void;
-  options: { id: number; value: string }[];
+  selected: Option;
+  setSelected: (selected: Option) => void;
+  options: Option[];
 }
-export function ListBox({ selected, setSelected, options }: ListBoxProps) {
+
+export type Option = {
+  value: string;
+};
+
+export function ListboxElement({
+  selected,
+  setSelected,
+  options,
+}: ListBoxProps) {
   return (
-    <Listbox
-      as="div"
-      className="relative w-full max-w-md"
-      value={selected}
-      onChange={setSelected}
-    >
-      <Listbox.Button className="border-san-marino-200 flex w-full items-center justify-between rounded-lg border  p-2.5 placeholder-zinc-400 ">
-        <span>{selected + 1}</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="14"
-          height="14"
-          viewBox="0 0 24 24"
-          className=" -rotate-90"
+    <Listbox value={selected} onChange={setSelected}>
+      <div className="relative ">
+        <Listbox.Button className=" border-1 relative w-max rounded   bg-zinc-900 px-4 py-2  pl-3 pr-10  text-left text-base  text-white hover:bg-zinc-800">
+          <span className="block truncate">{selected.value}</span>
+          <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+            <UpDownIcon />
+          </span>
+        </Listbox.Button>
+        <Transition
+          as={Fragment}
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" />
-        </svg>
-      </Listbox.Button>
-      <Transition
-        as={Fragment}
-        leave="transition ease-in duration-100"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-      >
-        <Listbox.Options
-          as="div"
-          className="absolute bottom-14 w-full  list-none rounded bg-white text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none "
-        >
-          {options.map((option) => (
-            <Listbox.Option
-              key={option.id}
-              value={option.id}
-              className={({ active }) =>
-                `relative cursor-default select-none py-2 pl-10 pr-2 ${
-                  active
-                    ? "bg-san-marino-100 text-san-marino-900"
-                    : "text-zinc-900"
-                }`
-              }
-            >
-              {({ selected }) => (
-                <>
-                  <span
-                    className={`block  truncate ${
-                      selected ? "font-medium" : "font-normal"
-                    }`}
-                  >
-                    {option.value}
-                  </span>
-                  {selected ? (
-                    <span className="absolute inset-y-0 left-0 flex items-center pl-3 ">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="14"
-                        height="14"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M9 22l-10-10.598 2.798-2.859 7.149 7.473 13.144-14.016 2.909 2.806z" />
-                      </svg>
+          <Listbox.Options className="absolute mt-1 max-h-60 w-max overflow-auto rounded-md bg-zinc-900 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            {options.map((option, index) => (
+              <Listbox.Option
+                key={index}
+                className={({ active }) =>
+                  `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                    active ? "bg-zinc-800 text-white" : "text-zinc-200"
+                  }`
+                }
+                value={option}
+              >
+                {({ selected }) => (
+                  <>
+                    <span
+                      className={`block truncate ${
+                        selected ? "font-medium" : "font-normal"
+                      }`}
+                    >
+                      {option.value}
                     </span>
-                  ) : null}
-                </>
-              )}
-            </Listbox.Option>
-          ))}
-        </Listbox.Options>
-      </Transition>
+                    {selected ? (
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-white">
+                        <CheckMark />
+                      </span>
+                    ) : null}
+                  </>
+                )}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </Transition>
+      </div>
     </Listbox>
+  );
+}
+
+function UpDownIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-6 w-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
+      />
+    </svg>
+  );
+}
+
+function CheckMark() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      className="h-5 w-5"
+    >
+      <path
+        fillRule="evenodd"
+        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+        clipRule="evenodd"
+      />
+    </svg>
   );
 }
