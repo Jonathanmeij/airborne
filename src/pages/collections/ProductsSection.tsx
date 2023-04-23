1233008196e0b54c0a562cf54591e0e2d04369fc
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-04-23 19:24:25 +0200

    added productsection

---
+++ src/pages/collections/ProductsSection.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Button, Container, SideMenu } from "~/components/ui";
+
+import { ListboxElement, type Option } from "~/components/ui/Input";
+
+// interface ProductOptionsProps {}
+
+export default function ProductsSection() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
